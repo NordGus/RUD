bba4dea9e90a649302d763f8a3c83eb8608b83da
Gustavo A. Rodriguez <gustavo.rodriguez92@gmail.com>
2022-06-04 20:31:39 +0200

    adding delays and effects on collision handler

--- Assets/Scripts/CollisionHandler.cs
+++ Assets/Scripts/CollisionHandler.cs
@@ -5,25 +5,61 @@ using UnityEngine.SceneManagement;
 
 public class CollisionHandler : MonoBehaviour
 {
+    private Movement mvComponent;
+    private Rigidbody rbComponent;
+
+    [SerializeField] float crashDelay = 1f;
+    [SerializeField] float nextLevelDelay = 1f;
+
+    void Start()
+    {
+        mvComponent = GetComponent<Movement>();
+        rbComponent = GetComponent<Rigidbody>();
+    }
+
     private void OnCollisionEnter(Collision collision)
     {
         switch (collision.gameObject.tag) 
         {
             case "Friendly":
-                Debug.Log("This is a friendly object");
                 break;
             case "Finish":
-                Debug.Log("Congrats you finish!");
+                StartFinishedSequence();
                 break;
             default:
-                ReloadScene();
+                StartCrashSequence();
                 break;
         }
+    }
+
+    private void StartCrashSequence()
+    {
+        // TODO: add crash SFX
+        // TODO: add crash particle effect
+        GetComponent<Movement>().enabled = false;
+        GetComponent<Rigidbody>().constraints = RigidbodyConstraints.None;
+        Invoke("ReloadScene", crashDelay);
+    }
+
+    private void StartFinishedSequence()
+    {
+        Invoke("LoadNextLevel", nextLevelDelay);
     }
 
     private void ReloadScene()
     {
         int currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
         SceneManager.LoadScene(currentSceneIndex);
+    }
+
+    private void LoadNextLevel()
+    {
+        int currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
+        int nextSceneIndex = currentSceneIndex + 1;
+
+        if (nextSceneIndex >= SceneManager.sceneCountInBuildSettings)
+            nextSceneIndex = 0;
+
+        SceneManager.LoadScene(nextSceneIndex);
     }
 }
