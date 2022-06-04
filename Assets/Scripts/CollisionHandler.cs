82c8065ded0bb4c78d13910faf5766b80fa33c01
Gustavo A. Rodriguez <gustavo.rodriguez92@gmail.com>
2022-06-04 09:40:40 +0200

    general cleaning

--- Assets/Scripts/CollisionHandler.cs
+++ Assets/Scripts/CollisionHandler.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class LandingSensor : MonoBehaviour
+public class CollisionHandler : MonoBehaviour
 {
     private bool landed;
 
