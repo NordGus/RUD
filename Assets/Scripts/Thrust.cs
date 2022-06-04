b096860148dd42c2cb33cacfad9ba09bf90b3ddd
Gustavo A. Rodriguez <gustavo.rodriguez92@gmail.com>
2022-06-04 07:04:47 +0200

    Adding Rocket engine sound effect
    Adding audio source to Rockect Prefab.
    Adding to project a rockect engine sound effect.
    Adding code to trigger the sound effect when boosting.

--- Assets/Scripts/Thrust.cs
+++ Assets/Scripts/Thrust.cs
@@ -5,20 +5,30 @@ using UnityEngine;
 public class Thrust : MonoBehaviour
 {
     private Rigidbody rigidbodyComponent;
+    private AudioSource audioSourceComponent;
+
     [SerializeField] float thrustPower = 1000f;
 
     // Start is called before the first frame update
     void Start()
     {
         rigidbodyComponent = GetComponent<Rigidbody>();
+        audioSourceComponent = GetComponent<AudioSource>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKey(KeyCode.Space)) {
+        if (Input.GetKey(KeyCode.Space))
+        {
+            if (!audioSourceComponent.isPlaying) audioSourceComponent.Play();
+            
             rigidbodyComponent.AddRelativeForce(CalculateThrustForce());
         }
+        else if (audioSourceComponent.isPlaying)
+        {
+            audioSourceComponent.Stop();
+        } 
     }
 
     private Vector3 CalculateThrustForce()
